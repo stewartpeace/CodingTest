215045fd0242cfdd88efa8209211498b8f62d71c
Stewart <stewartpeace@hotmail.com>
2016-10-30 20:28:06 +0000

    Added ISO checks for Currency Converter

--- SkeletonCode/CurrencyConverter/Converter.cs
+++ SkeletonCode/CurrencyConverter/Converter.cs
@@ -1,10 +1,25 @@
-﻿namespace SkeletonCode.CurrencyConverter
+﻿using System;
+using System.Collections.Generic;
+
+namespace SkeletonCode.CurrencyConverter
 {
 	public class Converter
 	{
+
+        List<string> ValidISOCodes = new List<string> { "GBP", "USD" };
+
 		public decimal Convert(string inputCurrency, string outputCurrency, decimal amount)
 		{
-			return amount;
+            if (!ValidISOCodes.Contains(inputCurrency))
+            {
+                throw new ArgumentException("Invalid ISO code", "inputCurrency");
+            }
+            else if (!ValidISOCodes.Contains(outputCurrency))
+            {
+                throw new ArgumentException("Invalid ISO code", "outputCurrency");
+            }
+            
+            return amount;
 		}
 	}
 }
